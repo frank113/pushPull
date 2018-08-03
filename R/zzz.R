33830dd094fe088a8be9bc8bf9e7c23dc69bdd49
Frank Kovacs <fkovacs@andrew.cmu.edu>
2018-08-03 11:01:44 -0400

    Updated R files

--- R/zzz.R
+++ R/zzz.R
@@ -1,46 +1,27 @@
-# .onLoad <- function(...) {
-#   ## function to prompt for the information needed from the user
-#   ask <- function(prompt, default = NULL) {
-#     if (!is.null(default) && length(default) > 0) {
-#       prompt <- paste0(prompt, " (or Enter for '", default, "')")
-#     }
-#     rtn <- readline(paste0(prompt, "? "))
-#     if (toupper(rtn) %in% c("Q", "QUIT"))
-#       stop("user quit setup()")
-#     if (!is.null(default) && rtn == "") {
-#       rtn <- default
-#     }
-#     return(rtn)
-#   }
-#   
-#   # Get new values for all parameters
-#   ## we need dummy variables, so we will use the dorms of frank
-#   sftpSite <- "scobell"
-#   sftpName <- "hamerschlag"
-#   sftpPassword <- "mudge_as_ra"
-#   userName <- "mudge_as_ca"
-#   cat("~~~Remember to run library(pushPull) if you need to change anything~~~\nWhat displays below cannot be changed, please ignore it\n")
-#   ## hide ask if not installed
-#   sftpSite <- ask("sftp site [not including sftp://]", sftpSite)
-#   sftpName <- ask("sftp login name", sftpName)
-#   sftpPassword <- ask("sftp password", sftpPassword)
-#   userName <- ask("Your user name", userName)
-#     
-#   ## store the information as variables
-#   ## use Howard's framework for saving as a dataframe
-#   key <- c("sftpSite", "sftpName", "sftpPassword", "userName")
-#   value <- c(sftpSite, sftpName, sftpPassword, userName)
-#   dtf <- data.frame(key = key, 
-#                     value = value)
-#   ## print("We made it to 35")
-#   if (file.exists("sysdata.rda")) {
-#     file.remove("sysdata.rda")
-#   }
-#   suppressMessages(devtools::use_data(dtf, 
-#                                       internal = TRUE, 
-#                                       overwrite = TRUE))
-#   ## print("We made it to 40")
-#   ## invisible(NULL)
-#   return()
-# }
-# 
+.onLoad <- function(...) {
+  ## need to get curl working
+  os <- Sys.info()["sysname"]
+  print("Made it to line 4")
+  if (os != "Windows") {
+    cat("If you do not have the correct cURL version, we will install it\n")
+    bash.profile <- system("cat ~/.bash_profile", 
+                           intern = TRUE)
+    if ( !('export PATH=\"/usr/local/opt/curl/bin:$PATH\"' %in% bash.profile) ) {
+      ## re-install libssh
+      cat("Re-installing libssh2\n")
+      system("brew install libssh2")
+      cat("Now installing curl with libssh2 support\n")
+      system("brew install curl --with-libssh2")
+      cat("Must now update the PATH\n")
+      ## todo: find a way to make sure that we dont keep putting it there
+      system("echo 'export PATH=\"/usr/local/opt/curl/bin:$PATH\"' >> ~/.bash_profile")
+    } else {
+      cat("You are good to go!\n")
+    }
+  } else {
+    cat("Add support for windows\n")
+  }
+  ## invisible(NULL)
+  return()
+}
+
